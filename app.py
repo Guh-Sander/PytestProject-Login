2288d8127333d626e5d6b5e364e5548597cee968
Gustavo-Sander <gustavosander@ienh.com.br>
2023-04-05 17:12:05 -0300

    Adicionando validações de usuário e senha

--- app.py
+++ app.py
@@ -7,12 +7,14 @@ def loginUser_input():
     A senha deve ter pelo menos 10 caracteres, um caracter especial, um número, ao menos uma letra maiúscula e uma letra minúscula.
     ''')
     user = input(str("Digite seu usuário: "))
+    return user
 
 # Solicita senha
 def loginPassword_input():
     password = input("Digite sua senha: ")
+    return password
 
-# Condições para o usuário e senha
+# Condições para a senha:
 def letra_mai(password):
     # Verifica se na senha pelo menos uma letra maiúscula.
     letra_maiuscula = False
@@ -23,9 +25,9 @@ def letra_mai(password):
             break  
     # Logo abaixo, o código mostra que se não for "True", print que deve ter uma letra maiúscula, se não apenas passa para o próximo comando.
     if not letra_maiuscula:
-        print("A senha deve conter pelo menos uma letra maiúscula.")
+        return False
     else:
-        pass
+        return True
 
 def letra_minu(password):
     # Verifica se na senha pelo menos uma letra minúscula.
@@ -37,9 +39,9 @@ def letra_minu(password):
             break
     # Logo abaixo, o código mostra que se não for "True", print que deve ter uma letra minúscula, se não apenas passa para o próximo comando.
     if not letra_minuscula:
-        print("A senha deve conter pelo menos uma letra minuscúla.")
+        return False
     else:
-        pass
+        return True
 
 def ver_num(password):
     # Verifica se a senha possui pelo menos algum número.
@@ -51,63 +53,70 @@ def ver_num(password):
             break
     # Logo abaixo, o código mostra que se não for "True", print que deve ter números, se não apenas passa para o próximo comando.
     if not possui_num:
-        print("A senha deve ter pelo menos um número.")
+        return False
     else:
-        pass
+        return True
 
 def caracter_senha(password):
     # Verifica se a senha não possui caracteres especiais.
     regex = re.compile('[^a-zA-Z0-9\s]')
     if not regex.findall(password):
-        print("A senha deve conter pelos menos um caracter especial.")
+        return False
     else:
-        pass
+        return True
 
 def min_caracter(password):
     # Verifica se a senha possui menos que 10 caracteres.
     if len(password) < 10:
-        print("A senha deve ter no minimo 10 caracteres.")
+        return False
     else:
-        pass
+        return True
 
+# Condições para o usuário:
 def max_trinta_caracter(user):
     # Verifica se o nome do usuário possui mais de trinta caracteres.
     if len(user) > 30:
-        print("Deve conter no máximo 30 carácter.")
+        return False
     else:
-        pass
+        return True
 
 def ver_espaco(user):
     # Verifica se contêm algum espaçamento no nome do usuário, passando letra por letra vendo se alguma delas é espaço.
     for letras in user:
         if letras.isspace():
-            print("Não deve conter espaçamento.")
+            return False
         else:
-            pass
+            return True
 
 def ver_caracter(user):
     # Verifica se tem caracteres especiais.
     regex = re.compile('[^a-zA-Z0-9\s]')
     if regex.findall(user):
-        print("Não deve conter caracteres especiais no usuário.")
+        return False
     else:
-        pass
+        return True
 
 def ver_maiuscula(user):
     # Verifica se a primeira letra "user[0]" não é maiúscula pelo comando "isupper()" que mostra se é maiúscula ou não, assim printando que deve ser maiúscula.
     if not user[0].isupper():
-        print("A primeira letra deve ser maiúscula.")
+        return False
     else:
         return True
 
-# Valida condições:
-def valida():
-    # Insere o usuário e senha
-    insereUser = loginUser_input()
-    inserePassword = loginPassword_input()
-    # condição do usuário
+# Valida condições do usuário:
+def valida_user():
+    while True:
+        insereUser = loginUser_input()
+        if valida_user(insereUser) and ver_maiuscula(insereUser) and ver_caracter(insereUser) and ver_espaco(insereUser) and max_trinta_caracter(insereUser):
+            break
+        else:
+            print("Usuário inválido! Deve começar com letra maiúscula, sem caracteres especiais e sem espaços, com no máximo 30 caracteres.")
 
-    # condição da senha 
-    cond1_password = letra_mai(inserePassword)
-    print(cond1_password)
-valida()
+# Valida condições da senha:
+def valida_password():
+    while True:
+        inserePassword = loginPassword_input()
+        if valida_password(inserePassword) and min_caracter(inserePassword) and caracter_senha(inserePassword) and ver_num(inserePassword) and letra_minu(inserePassword) and letra_mai(inserePassword):
+            break
+        else:
+            print("Senha inválida! Deve ter pelo menos 10 caracteres, um caracter especial, um número, ao menos uma letra maiúscula e uma letra minúscula.")
